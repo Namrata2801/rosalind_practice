311250224ed7db4f1bb20c9e534bc7741c9e0602
Namrata2801 <namrata.7838@gmail.com>
2022-04-23 17:11:03 +0530

    Add ini3.py

---
+++ ini3.py
@@ -0,0 +1,16 @@
+"""sfDQ2GbDCnrCHVKWfusLKOvisnprkjacksoniBRZXjXtKcjNgP5ryQ28At2hgNkrA5JM90RgNgF0AhIXXLrgAgK3yUgVr5uX88ySFCM0Pf73JvUZszs1OckkkwvQuOkuGOG8gblm4936QcPKyHQyd0mCwlUppFjLCVYFiKLlbM0uy1FLGVPMfbGc2trx7x5ij33wfp.
+21 24 29 36"""
+
+# with open("../rosalind_ini3.txt", "r") as f:
+#     lines = f.readlines()
+# st = lines[0]
+# a, b, c, d = lines[1].strip().split()
+
+# print(st[int(a): int(b)+1], end = " ")
+# print(st[int(c): int(d)+1])
+
+s= "sfDQ2GbDCnrCHVKWfusLKOvisnprkjacksoniBRZXjXtKcjNgP5ryQ28At2hgNkrA5JM90RgNgF0AhIXXLrgAgK3yUgVr5uX88ySFCM0Pf73JvUZszs1OckkkwvQuOkuGOG8gblm4936QcPKyHQyd0mCwlUppFjLCVYFiKLlbM0uy1FLGVPMfbGc2trx7x5ij33wfp"
+a = s[21:25]
+b = s[29:37]
+print(a, end=" ")
+print(b)
